922ec7d97589734d06671f981ac1a98c3562d7ea
Begzod <dusmamatov46@gmail.com>
2022-11-14 20:09:33 +0500

    timurs work

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ let contents = document.querySelectorAll('.content')
 console.log(buttons);
 buttons.forEach(btn => {
 	btn.onclick = () => {
+		if(btn.getAttribute('data-p') === 'newsletter') {
+			document.body.style.background = "rgb(40, 40, 40)"
+		}
 		contents.forEach(page => {
 			if (page.id === btn.getAttribute('data-p')) {
 				contents.forEach(el => el.classList.remove('activePage'))
