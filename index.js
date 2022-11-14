63793e46d245eff1e8fe7d89ea0f5d5d60268e1b
djurakulov14 <109537648+djurakulov14@users.noreply.github.com>
2022-11-14 17:05:54 +0500

    newsletter

--- index.js
+++ index.js
@@ -1,20 +1,3 @@
-
-// let buttons = document.querySelectorAll('nav a')
-// let contents = document.querySelectorAll('.content')
-
-
-// buttons.forEach(btn => {
-// 	btn.onclick = () => {
-// 		contents.forEach(page => {
-// 			if (page.id === btn.getAttribute('data-p')) {
-// 				contents.forEach(el => el.classList.remove('activePage'))
-// 				page.classList.add('activePage')
-// 			}
-// 		})
-// 	}
-// })
-
-
 let buttons = document.querySelectorAll('.btns')
 let contents = document.querySelectorAll('.content')
 
